e3a6cbf76648e4b5cc8202853a95c2c3dd3de8da
melinda.sava <melinda.sava@abax.com>
2021-09-29 19:16:01 +0000

    customized matherial theme

---
+++ src/components/MaterialTheme/index.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0A8B74',
+      darker: '#075f4f',
+    },
+    neutral: {
+      main: '#ebf4f3',
+      contrastText: '#212121',
+    },
+  },
+});
